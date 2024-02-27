4c4ab45d85ef482996b747c25da1728e7034f272
JoshuaMuhdDullah <146445696+JoshuaMuhdDullah@users.noreply.github.com>
2024-02-27 22:14:48 +0000

    27/02/24

---
+++ MazeGame/SkeletonCode.c
@@ -0,0 +1,68 @@
+// Struct
+struct Maze {
+    // Player position: array of 2 elements ([row,column])
+    // Maze: Arrays of arrays (holds the all elements of the maze)
+}
+
+// File opening
+FILE openFile(char *filename) {
+
+}
+
+// Inititialize maze
+Maze initializeMaze() {
+    openFile()
+    // Read File
+}
+
+// Display maze
+void displayMaze() {
+    // Loop through rows
+        // Loop through columns
+            // Check if end of row
+                // Print newline
+
+            // Print out current element of maze
+}
+
+// Player movement
+int playerMovement() {
+    // Check for keyboard input
+        // If W has been pressed
+            // Wall in way
+                // Dont change player position
+            // Wall not in way
+                // Change player position
+
+        // If A has been pressed
+            // Wall in way
+                // Dont change player position
+            // Wall not in way
+                // Change player position
+
+        // If S has been pressed
+            // Wall in way
+                // Dont change player position
+            // Wall not in way
+                // Change player position
+
+        // If D has been pressed
+            // Wall in way
+                // Dont change player position
+            // Wall not in way
+                // Change player position
+}
+
+// Check win condition
+int winCondition{
+    // If player position is equal to end position
+        //return 1
+
+    // If player position isn't equal to end position
+        // return 0
+}
+
+// Maze game
+int main() {
+    struct Maze myMaze;
+}
